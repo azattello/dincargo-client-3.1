d114d733c895d3ca9e5049ce34519fc080a64a82
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-18 15:33:50 +0500

    din cl 3.1

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://din-cargo.kz:3001',
+    
+    nameCargo: 'Din-Cargo'
+  };
+  
+  
+export default config;
+  
